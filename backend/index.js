c706ab015a3973eed0895c2b8221c353e1015f6d
mitmit-22 <sumitjiashigurung@gmail.com>
2026-06-11 13:41:56 +0530

    updated the .env frontend url

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
+  credentials: true //change
 }))
 
 app.use(express.json());
